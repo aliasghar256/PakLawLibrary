3fea6d41c48ac8441192a13b12283bbc44c40c0a
Hasan_Ali_Vejlani <hasanalivejlani@gmail.com>
2024-06-04 20:11:27 +0500

    Dashboard display fixed

--- src/components/dashboard/dashboardheader/DashboardHeader.jsx
+++ src/components/dashboard/dashboardheader/DashboardHeader.jsx
@@ -1,40 +1,20 @@
-// export default function DashboardHeader() {
-//   return (
-//     <>
-//       <header>
-//         <nav>
-//           <ul>
-//             <li>
-//               <a href="/dashboard">dashboard</a>
-//             </li>
-//             <li>
-//               <a href="/bookmarks">bookmarks</a>
-//             </li>
-//           </ul>
-//         </nav>
-//       </header>
-//     </>
-//   );
-// }
-
 import "./DashboardHeader.css";
 
-import { AppBar, Tabs, Tab, Link } from "@mui/material";
-
 export default function DashboardHeader() {
-  const handleTabChange = (event, newValue) => {
-    // Handle tab change logic (optional)
-  };
-
   return (
-    <AppBar position="static">
-      <Tabs value={0} onChange={handleTabChange}>
-        <Tab component={Link} href="/dashboard" label="Dashboard" />
-        <Tab component={Link} href="/bookmarks" label="Bookmarks" />
-      </Tabs>
-    </AppBar>
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="/dashboard">Dashboard</a>
+            </li>
+            <li>
+              <a href="/bookmarks">Bookmarks</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
-
-
-
