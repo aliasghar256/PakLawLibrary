480b20cefa51b68248d120902705b350bab8f1fc
Ali Asghar Hussain <alih11737@gmail.com>
2024-06-04 18:33:30 +0500

    context & router pending

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -7,14 +7,20 @@ import AdvancedSearch from "./searchbars/AdvancedSearch";
 
 export default function Dashboard({ userData, setUserData }) {
   const [searchBarIndex, setSearchBarIndex] = useState(0);
-  console.log(userData);
+  const [query, setQuery] = useState({});
+  console.log("User Data from Dash: ", userData);
+  console.log("Query from Dash: ", query);
   return (
     <>
       <DashboardHeader />
       <h1>Dashboard</h1>
-      {searchBarIndex === 0 && <JudgmentSearch />}
-      {searchBarIndex === 1 && <AdvancedSearch />}
-      <SearchOptions setSearchBarIndex={setSearchBarIndex} />
+      {searchBarIndex === 0 && (
+        <JudgmentSearch token={userData.token} setQuery={setQuery} />
+      )}
+      {searchBarIndex === 1 && (
+        <AdvancedSearch token={userData.token} setQuery={setQuery} />
+      )}
+      <SearchOptions setSearchBarIndex={setSearchBarIndex} query={query} />
       <SearchResults searchBarIndex={searchBarIndex} />
     </>
   );
