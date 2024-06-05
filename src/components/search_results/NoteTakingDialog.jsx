1960fd1c9ca133a835fedae841b4db69b7185afa
Ali Asghar Hussain <alih11737@gmail.com>
2024-06-05 16:11:23 +0500

    Final

--- src/components/search_results/NoteTakingDialog.jsx
+++ src/components/search_results/NoteTakingDialog.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import {
   Dialog,
   DialogTitle,
@@ -8,9 +8,54 @@ import {
   TextField,
 } from "@mui/material";
 import axios from "axios";
+import Toastify from "toastify-js";
+import "toastify-js/src/toastify.css";
+import UserContext from "../../UserContext";
 
 export default function NoteTakingDialog({ open, onClose, judgmentID }) {
   const [newNote, setNewNote] = useState("");
+  const { userData } = useContext(UserContext);
+
+  useEffect(() => {
+    const fetchNote = async () => {
+      try {
+        let url = "http://127.0.0.1:3001/usernote/view";
+        const response = await axios.get(url, {
+          headers: {
+            Authorization: `Bearer ${userData.token}`,
+            judgmentid: judgmentID,
+          },
+        });
+        if (response.status === 200) {
+          setNewNote(response.data.note.note);
+          Toastify({
+            text: "Note Found Successful",
+
+            duration: 2000,
+
+            style: {
+              background: "linear-gradient(to right, #00b09b, #96c93d)",
+            },
+          }).showToast();
+        }
+      } catch (error) {
+        console.error("No previous Notes:", error);
+        Toastify({
+          text: error,
+
+          duration: 2000,
+
+          style: {
+            background: "linear-gradient(to right, #c30010, #f94449)",
+          },
+        }).showToast();
+      }
+    };
+
+    if (open) {
+      fetchNote();
+    }
+  }, []);
 
   const handleAddNote = async () => {
     if (newNote.trim() !== "") {
@@ -20,17 +65,35 @@ export default function NoteTakingDialog({ open, onClose, judgmentID }) {
           { note: newNote },
           {
             headers: {
-              Authorization:
-                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFAZ21haWwuY29tIiwiaWQiOiI2NWNmNWYxNWU5OTE3MTE3OWEwNTlkMTYiLCJpYXQiOjE3MTc0Nzg1OTcsImV4cCI6MTcxNzU2NDk5N30.jhk8dqGmcc0nRy8VusnoCPwDX-DmodAkUYeQ1Q44oN8",
+              Authorization: `Bearer ${userData.token}`,
               judgmentid: judgmentID,
             },
           }
         );
-        console.log("Note saved:", response.data);
-        setNewNote("");
-        onClose();
+        // console.log("Note saved:", response.data);
+        if (response.status === 200) {
+          Toastify({
+            text: "Note Saved Successful",
+
+            duration: 2000,
+
+            style: {
+              background: "linear-gradient(to right, #00b09b, #96c93d)",
+            },
+          }).showToast();
+        }
+
       } catch (error) {
         console.error("Error saving note:", error);
+        Toastify({
+          text: error,
+
+          duration: 2000,
+
+          style: {
+            background: "linear-gradient(to right, #c30010, #f94449)",
+          },
+        }).showToast();
       }
     }
   };
